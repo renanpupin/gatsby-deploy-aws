f295ce78294e1ac14751a0c46a3d882c19ebe236
pedro pupin <pedro.phzp@hotmail.com>
2020-12-02 11:08:12 -0300

    Update actions.spec.js

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,12 +1,14 @@
-/// <reference types="cypress" />
-
 context('Actions', () => {
-  beforeEach(() => {
+  // beforeEach(() => {
+  //   cy.visit('https://example.cypress.io/commands/actions')
+  // })
+
+  it('Visit', () => {
     cy.visit('https://example.cypress.io/commands/actions')
   })
 
-  it('.click() - click on a DOM element', () => {
-    // https://on.cypress.io/click
-    cy.get('.action-btn').click()
-  })
+  // it('.click() - click on a DOM element', () => {
+  //   // https://on.cypress.io/click
+  //   cy.get('.action-btn').click()
+  // })
 })
